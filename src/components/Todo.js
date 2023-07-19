6df0cac782d0859e8375b38cd81cb85b9c3de830
1156953217@qq.com <1156953217@qq.com>
2023-07-19 15:49:33 +0800

    创建组件

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+function Todo(){
+    return (
+        <div className="card">
+        <h2>TITLE</h2>
+        <div class='action'>
+            <span> A span</span>
+          <button className="btn">DELETE</button>
+        </div>
+      </div>
+    );
+}
+
+
+export default Todo;

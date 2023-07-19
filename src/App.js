e742e2883f190fe7f898fc36ac66a492cbb6a855
1156953217@qq.com <1156953217@qq.com>
2023-07-19 17:09:15 +0800

    引入 useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import Backdrop from "./components/Backdrop";
-import Modal from "./components/Modal";
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo text= 'learn react'/>
-      <Todo text= 'master react'/>
-      <Todo text= 'explore react'/>
-      <Modal />
-      <Backdrop />
+      <Todo text="learn react" />
+      <Todo text="master react" />
+      <Todo text="explore react" />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,46 @@
-//prop的作用是传递文本值
-function Todo(props){
-  function deleteHandler(){
-    //这三个不同的按钮会打印不同的日志
-    console.log('Clicked!');
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
+// React 组件是返回标签的 JavaScript 函数，React 组件必须以大写字母开头
+// React 组件必须以大写字母开头
+function Todo(props) {
+  // 从 React 引入 useState 后，在组件中声明一个 state 变量
+  const [modalIsopen, setModalIsopen] = useState(false);
+  // 惯例，需要像这样 [something, setSomething] 为它们命名
+  const [count, setCount] = useState(0);
+
+  function deleteHandler() {
+    // 这三个不同的按钮会打印不同的日志
+    console.log("Clicked!");
     console.log(props.text);
+    setModalIsopen(true);
   }
-  //JSX 会让你把标签放到 JavaScript 中。而大括号会让你 “回到” JavaScript 中
+
+  function handleClick() {
+    // 点击该按钮计数器将递增
+    setCount(count + 1);
+    console.log(count);
+  }
+  // JSX 会让你把标签放到 JavaScript 中。而大括号会让你 “回到” JavaScript 中
   return (
-      <div className="card">
-        <h2>{props.text}</h2>
-        <div className='action'>
-          <button className="btn" onClick={deleteHandler}>DELETE</button>
+    <div className="card">
+      {/* prop 的作用是传递文本值 */}
+      <h2>{props.text}</h2>
+      {/* 在组件中声明 事件处理 函数来响应事件，注意结尾没有小括号*/}
+      <div className="action">
+        <button className="btn" onClick={deleteHandler}>
+          DELETE
+        </button>
+        <button onClick={handleClick}>Clicked {count} times</button>
       </div>
+
+      {/* if modalIsopen is true then render the modal */}
+      {modalIsopen ? <Modal /> : null}
+      {/* if modalIsopen is true then render the Backdrop */}
+      {modalIsopen && <Backdrop />}
     </div>
   );
 }
-
 
 export default Todo;

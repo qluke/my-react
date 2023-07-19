579efa5b63d5f197fc836c30fa1008ce4e73c823
1156953217@qq.com <1156953217@qq.com>
2023-07-19 18:01:07 +0800

    将函数作为参数传递

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,7 +6,7 @@ import Backdrop from "./Backdrop";
 // React 组件必须以大写字母开头
 function Todo(props) {
   // 从 React 引入 useState 后，在组件中声明一个 state 变量
-  const [modalIsopen, setModalIsopen] = useState(false);
+  const [modalIsopen, setModalIsOpen] = useState(false);
   // 惯例，需要像这样 [something, setSomething] 为它们命名
   const [count, setCount] = useState(0);
 
@@ -14,9 +14,12 @@ function Todo(props) {
     // 这三个不同的按钮会打印不同的日志
     console.log("Clicked!");
     console.log(props.text);
-    setModalIsopen(true);
+    setModalIsOpen(true);
   }
 
+  function closeModalHandler(){
+    setModalIsOpen(false);
+  }
   function handleClick() {
     // 点击该按钮计数器将递增
     setCount(count + 1);
@@ -27,7 +30,7 @@ function Todo(props) {
     <div className="card">
       {/* prop 的作用是传递文本值 */}
       <h2>{props.text}</h2>
-      {/* 在组件中声明 事件处理 函数来响应事件，注意结尾没有小括号*/}
+      {/* 在组件中声明 “事件处理” 函数来响应事件，注意结尾没有小括号*/}
       <div className="action">
         <button className="btn" onClick={deleteHandler}>
           DELETE
@@ -36,9 +39,10 @@ function Todo(props) {
       </div>
 
       {/* if modalIsopen is true then render the modal */}
-      {modalIsopen ? <Modal /> : null}
+      {modalIsopen ? <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/> : null}
       {/* if modalIsopen is true then render the Backdrop */}
-      {modalIsopen && <Backdrop />}
+      {/* 将函数作为参数传递 */}
+      {modalIsopen && <Backdrop onCancel={closeModalHandler}/>}
     </div>
   );
 }
